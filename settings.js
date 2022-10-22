01a121ac3e74771415bd04128457aeda2f5f789b
sampaththaru <92896750+sampaththaru@users.noreply.github.com>
2022-10-22 15:15:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94772880525'] //ur owner number
+global.ownername = "*💫🪄⃟Tharuindu😘🍃⃟✨✦⃝Sampath🌼᭄❤️༻♥§꧂⚡*" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94772880525']
+global.ownertag = '94772880525' //ur tag number
+global.botnumber = '94772880525' //Add your Bot Number For Log
+global.botname = '*💫🪄⃟Tharuindu😘🍃⃟✨✦⃝Sampath🌼᭄❤️༻♥§꧂⚡*' //ur bot name
+global.fb = "https://www.facebook.com/profile.php?id=100045403656731" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.facebook.com/profile.php?id=100045403656731" //ur website to be displayed
+global.sc = 'https://www.facebook.com/profile.php?id=100045403656731' //script link
+global.linkz = 'https://www.facebook.com/profile.php?id=100045403656731'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "*Tharu_Bot*" //ur sticker watermark packname
+global.author = "*💫🪄⃟Tharuindu😘🍃⃟✨✦⃝Sampath🌼᭄❤️༻♥§꧂⚡*" //ur sticker watermark author
+global.footer = "*Created By ❤️⃝➤⃟Tharu❤️⃟⃟❀Bot🍃⃟✦⃝࿔*"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
